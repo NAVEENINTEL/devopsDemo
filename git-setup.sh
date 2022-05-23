f8e8c2e879a4d28fe35dfaa5fbd6cd006d2b6f0e
Naveen Alate <naveenintel86@gmail.com>
2022-05-23 11:54:52 +0530

    Updating git-setup.sh shell script

--- git-setup.sh
+++ git-setup.sh
@@ -2,11 +2,19 @@
 #print the  value from variable
 echo  "INSTALLING GIT LOCALLY"
 # updating softwares
-sudo apt-get update 
+sudo apt-get update -y
 # installing GIT using apt 
-sudo apt-get install git
-#verifying the GIT installation 
+echo "INSTALLING GIT "
+sudo apt-get install git -y
+echo "verifying the GIT installation" 
 git --version
-
-#git config --global user.name "Emma Paris"
-#git config --global user.email "eparis@atlassian.com"
+#checking if any user ,email configured
+echo "DSIPLAYING CONFIGURATION LIST:"
+git config --list
+#Configure username and email ,when new changes made by you,all teams memebers can easily identify by you name,email
+echo "Enter your name."
+read name
+echo "Enter your email used for GIT account."
+read email
+git config --global user.name  $name
+git config --global user.email  $email
